e3719287cb15935a1f53c5554f001aa8c5414051
Rishard Davis <rrdavis777@gmail.com>
2022-02-23 13:08:26 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(part1 = "special") {
+            return `You are ${flair}${part1}${flair}!`;
+    }
+}
